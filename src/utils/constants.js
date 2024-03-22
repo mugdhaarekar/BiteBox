5807a829ebb895e426f9442b7ebc697e02a19f55
Mugdha <mugdha@kredily.com>
2024-03-22 16:05:16 +0530

    FEAT: initialSetup Swiggy clone with Simmer UI

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const CDN_URL =
+  "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+export const LOGO_URL =
+  "https://cdn.dribbble.com/users/5980575/screenshots/15184438/media/59937b02fb210a47a26d24b6b0caba08.png?resize=400x300&vertical=center";
